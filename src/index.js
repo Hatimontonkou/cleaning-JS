100c0639a78736968a2129482352b78c002dfbc4
Hatimontonkou <110019666+Hatimontonkou@users.noreply.github.com>
2022-10-07 01:49:23 +0900

    追加機能（スーツ類）

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+  document.getElementById("incomplete-suit-list").appendChild(div);
+};
+
+document
+  .getElementById("add-suit")
+  .addEventListener("click", () => onClickAdd());
