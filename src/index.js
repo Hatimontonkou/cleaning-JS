a8c922152fb4bf61820af7acdcfc34c88f5098d9
Hatimontonkou <110019666+Hatimontonkou@users.noreply.github.com>
2022-10-09 00:28:26 +0900

    追加機能ボタン

--- src/index.js
+++ src/index.js
@@ -10,7 +10,21 @@ const onClickSuit = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "クリーニング済み";
+  doneButton.addEventListener("click", () => {
+    alert("クリーニング済み");
+  });
+
+  const cancel = document.createElement("button");
+  cancel.innerText = "キャンセル";
+  cancel.addEventListener("click", () => {
+    alert("キャンセル");
+  });
+
   div.appendChild(li);
+  div.appendChild(doneButton);
+  div.appendChild(cancel);
   document.getElementById("just-cleaning-suit-list").appendChild(div);
 };
 
@@ -24,7 +38,21 @@ const onClickFuton = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "クリーニング済み";
+  doneButton.addEventListener("click", () => {
+    alert("クリーニング済み");
+  });
+
+  const cancel = document.createElement("button");
+  cancel.innerText = "キャンセル";
+  cancel.addEventListener("click", () => {
+    alert("キャンセル");
+  });
+
   div.appendChild(li);
+  div.appendChild(doneButton);
+  div.appendChild(cancel);
   document.getElementById("just-cleaning-futon-list").appendChild(div);
 };
 
@@ -38,7 +66,21 @@ const onClickElse = () => {
   const li = document.createElement("li");
   li.innerText = inputtext;
 
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "クリーニング済み";
+  doneButton.addEventListener("click", () => {
+    alert("クリーニング済み");
+  });
+
+  const cancel = document.createElement("button");
+  cancel.innerText = "キャンセル";
+  cancel.addEventListener("click", () => {
+    alert("キャンセル");
+  });
+
   div.appendChild(li);
+  div.appendChild(doneButton);
+  div.appendChild(cancel);
   document.getElementById("just-cleaning-else-list").appendChild(div);
 };
 
