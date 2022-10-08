4052953c122c5ea9fb33054ee31eec0a18d8aaac
Hatimontonkou <110019666+Hatimontonkou@users.noreply.github.com>
2022-10-08 15:51:06 +0900

    追加機能（布団類、その他）

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-const onClickAdd = () => {
+const onClickSuit = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
@@ -11,9 +11,45 @@ const onClickAdd = () => {
   li.innerText = inputText;
 
   div.appendChild(li);
-  document.getElementById("incomplete-suit-list").appendChild(div);
+  document.getElementById("just-cleaning-suit-list").appendChild(div);
+};
+
+const onClickFuton = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+  document.getElementById("just-cleaning-futon-list").appendChild(div);
+};
+
+const onClickElse = () => {
+  const inputtext = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputtext;
+
+  div.appendChild(li);
+  document.getElementById("just-cleaning-else-list").appendChild(div);
 };
 
 document
   .getElementById("add-suit")
-  .addEventListener("click", () => onClickAdd());
+  .addEventListener("click", () => onClickSuit());
+
+document
+  .getElementById("add-futon")
+  .addEventListener("click", () => onClickFuton());
+
+document
+  .getElementById("add-else")
+  .addEventListener("click", () => onClickElse());
